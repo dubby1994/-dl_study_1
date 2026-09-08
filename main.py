876af5ec6834bb40a598cab40daba13a3a9c4342
dubby <dubby@duby.cn>
2026-09-08 10:55:40 +0800

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+import torch
+
+print(f"torch version: {torch.__version__}")
+print(f"cuda available: {torch.cuda.is_available()}")
+print(f"cuda count: {torch.cuda.device_count()}")
+print(f"cuda name: {torch.cuda.get_device_name(0)}")
