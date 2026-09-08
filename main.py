54c1700bc3ae4f845beb6de68f6a79dc4ac2d0ca
dubby <dubby@duby.cn>
2026-09-08 11:15:30 +0800

    fix

--- main.py
+++ main.py
@@ -4,6 +4,3 @@ print(f"torch version: {torch.__version__}")
 print(f"cuda available: {torch.cuda.is_available()}")
 print(f"cuda count: {torch.cuda.device_count()}")
 print(f"cuda name: {torch.cuda.get_device_name(0)}")
-
-
-print("hello world")
